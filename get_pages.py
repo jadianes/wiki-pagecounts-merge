0153a2aa0c2fb2010162be7147d1dad2b5a78d47
Jose A Dianes <jadianes@gmail.com>
2015-03-05 18:12:54 +0000

    initial commit

---
+++ get_pages.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+from urllib2 import urlopen, URLError, HTTPError
+
+url_root = 'https://dumps.wikimedia.org/other/pagecounts-raw'
+
+def download_file(url, target_folder):
+    # Open the url
+    try:
+        f = urlopen(url)
+        print "downloading " + url
+
+        # Open our local file for writing
+        with open(os.path.join(target_folder, os.path.basename(url)), "wb") as local_file:
+            local_file.write(f.read())
+
+        return local_file
+
+    #handle errors
+    except HTTPError, e:
+        print "HTTP Error:", e.code, url
+    except URLError, e:
+        print "URL Error:", e.reason, url
+
+# Example link: https://dumps.wikimedia.org/other/pagecounts-raw/2009/2009-05/pagecounts-20090501-000000.gz
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target_path", help="A path to download all the files")
+    parser.add_argument("year", help="The year to download page counts as a number", type=int)
+    parser.add_argument("month", help="The month to download page counts as a number", type=int)
+    parser.add_argument("from_day", help="The starting day to download page counts as a number", type=int)
+    parser.add_argument("to_day", help="The ending day to download page counts as a number", type=int)
+    args = parser.parse_args()
+
+    # Iterate over image ranges
+    for day in range(args.from_day, args.to_day+1):
+        for hour in range(0,23):
+            url_to_download = (url_root + "/%d/%d-%02d/pagecounts-%d%02d%02d-%02d0000.gz" %
+                               (args.year, args.year, args.month, args.year, args.month, day, hour))
+            print "Preparing to download ", url_to_download
+            # Create destination folder if needed
+            if not os.path.exists(args.target_path):
+                os.makedirs(args.target_path)
+            elif not os.path.isdir(args.target_path):
+                print("Target folder is not a directory")
+                exit()
+            # Finally download the file
+            download_file(url_to_download, args.target_path)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ merge_pages.py
@@ -0,0 +1,33 @@
+import argparse, os, gzip
+import re
+
+file_name_pattern = r'pagecounts-\d{8}-\d{6}.gz'
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_folder", help="The folder containing the input files")
+    parser.add_argument("target_file", help="The file where the result will be written. "
+                                            "If the files exists, contents will be appended")
+    args = parser.parse_args()
+
+    # open target file for write or append
+    if not os.path.exists(args.target_file):
+        target_f = open(args.target_file, 'w')
+    else:
+        target_f = open(args.target_file, 'a')
+
+    # here we scan every file, appending the timestamp part of the filename to the new lines in the target
+    for a_file in os.listdir(args.input_folder):
+        if re.match(file_name_pattern,a_file):
+            # we have our file
+            f = gzip.open(os.path.join(args.input_folder, a_file), 'rb')
+            file_content = f.readlines()
+            for line in file_content:
+                new_line = a_file[11:-3] + ' ' + line
+                target_f.write(new_line)
+
+    target_f.close()
+
+if __name__ == '__main__':
+    main()
