85f48696dbc32826d4bfa963942feb8ea24d30eb
Jose A Dianes <jadianes@gmail.com>
2015-03-06 08:42:03 +0000

    sorting files before merge

--- merge_pages.py
+++ merge_pages.py
@@ -18,7 +18,7 @@ def main():
         target_f = open(args.target_file, 'a')
 
     # here we scan every file, appending the timestamp part of the filename to the new lines in the target
-    for a_file in os.listdir(args.input_folder).sort():
+    for a_file in sorted(os.listdir(args.input_folder)):
         if re.match(file_name_pattern,a_file):
             print "Merging file ", a_file, "..."
             # we have our file
